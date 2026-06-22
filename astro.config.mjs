517079d418c130bf6c58997c90b73a829dc06a6f
kathhill <kath.hill@sbcglobal.net>
2026-06-22 16:46:58 -0700

    feat: Use postcss with tailwind 4.3 instead of vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,30 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/postcss";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:pixelhillpublishing.com',    
+  site: 'https:pixelhillpublishing.com',
+
   integrations: [
     sitemap(),
+    icon(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
       },
     }),
   ],
+
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
+    },
+  },
 });
